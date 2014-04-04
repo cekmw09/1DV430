fbd5a85ca2c0753149d9267963ab8ecc086fb1e8
cekmw09 <info@snabbsurfa.se>
2014-04-04 13:37:47 +0200

    Skapade en funktion som renderar logotypen

--- inc/javascript/Mastermind.js
+++ inc/javascript/Mastermind.js
@@ -4,7 +4,14 @@ var Mastermind = {
 	gameBoard : document.getElementById("gameBoard"),
 	
 	init: function (e) {
-		
+		Mastermind.renderLogo();
+	},
+	
+	renderLogo: function() {
+		var logo = document.createElement("img");
+		logo.src = "inc/graphics/logo.png";
+		logo.alt = "Mastermind";
+		gameBoard.appendChild(logo);
 	}
 }
 
