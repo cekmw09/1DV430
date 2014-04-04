0de12defa28b78932ee3b7ce4870034434a9c3e4
cekmw09 <info@snabbsurfa.se>
2014-04-04 14:03:43 +0200

    Skapade en funktion som renderar huvudmenyn

--- inc/javascript/Mastermind.js
+++ inc/javascript/Mastermind.js
@@ -5,6 +5,7 @@ var Mastermind = {
 	
 	init: function (e) {
 		Mastermind.renderLogo();
+		Mastermind.renderMenu();
 	},
 	
 	renderLogo: function() {
@@ -12,6 +13,41 @@ var Mastermind = {
 		logo.src = "inc/graphics/logo.png";
 		logo.alt = "Mastermind";
 		gameBoard.appendChild(logo);
+	},
+	
+	renderMenu: function () {
+		var menu = document.createElement("ul");
+		var optionLi, optionA, optionText;
+		var options = new Array(
+			"Play",
+			"Settings",
+			"Help",
+			"Credits"
+		);
+		
+		menu.setAttribute("id", "menu");
+		
+		for(var option in options) {
+			optionText = document.createTextNode(options[option]);
+			optionA = document.createElement("a");
+			optionLi = document.createElement("li");
+			
+			optionA.href = "#";
+			
+			optionA.appendChild(optionText);
+			optionLi.appendChild(optionA);
+			menu.appendChild(optionLi);
+			
+			optionA.onmouseover = function() {
+				
+			}
+			
+			optionA.onclick = function() {
+				
+			}
+		}
+		
+		gameBoard.appendChild(menu);
 	}
 }
 
