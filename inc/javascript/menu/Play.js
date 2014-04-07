01a47af0f50fa4627b40354aec6321c975dee11f
cekmw09 <info@snabbsurfa.se>
2014-04-07 14:01:05 +0200

    Skapade metod som jämför två arrayer

--- inc/javascript/menu/Play.js
+++ inc/javascript/menu/Play.js
@@ -11,3 +11,25 @@ Play.prototype.secretCode = function() {
 	}
 	return out;
 }
+
+Play.prototype.compareArrays = function(arr1, arr2) {
+	var correct = 0, exists = 0, out = new Array();
+	
+	for(var i=0; i < arr1.length; i++) {
+		if(arr1[i] === arr2[i])
+			correct++;
+	}
+	
+	if(arr1[0] === arr2[1] || arr1[0] === arr2[2] || arr1[0] === arr2[3])
+		exists++;
+	if(arr1[1] === arr2[0] || arr1[1] === arr2[2] || arr1[1] === arr2[3])
+		exists++;
+	if(arr1[2] === arr2[0] || arr1[2] === arr2[1] || arr1[2] === arr2[3])
+		exists++;
+	if(arr1[3] === arr2[0] || arr1[3] === arr2[1] || arr1[3] === arr2[2])
+		exists++;
+		
+	out = [correct, exists];
+	
+	return out;
+}
