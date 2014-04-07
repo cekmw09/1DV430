7c2b32ccebee8b3a419ddad3462c68c38693ad34
cekmw09 <info@snabbsurfa.se>
2014-04-07 10:13:27 +0200

    Justerade felaktigt värde

--- inc/javascript/menu/Play.js
+++ inc/javascript/menu/Play.js
@@ -6,7 +6,7 @@ function Play() {
 
 Play.prototype.secretCode = function() {
 	var i = 7, out = new Array();
-	for(var j = 0; j < i; j++) {
+	for(var j = 0; j < 4; j++) {
 		out[j] = Math.floor(Math.random() * (i + 1));
 	}
 	return out;
