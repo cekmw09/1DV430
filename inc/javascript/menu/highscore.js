a9ea34577b2c373f9c261bae648705bf01c3cede
cekmw09 <info@snabbsurfa.se>
2014-05-06 17:20:39 +0200

    Formatering i highscore

--- inc/javascript/menu/highscore.js
+++ inc/javascript/menu/highscore.js
@@ -2,7 +2,31 @@
 
 var Mastermind = Mastermind || {};
 
-Mastermind.highscore = function (result, time) {
+Mastermind.highscore = function (timer) {
     var frame = document.getElementById("frame");
+    var d = new Date();
+    var now = d.getTime();
+
+    var form = document.createElement("form");
+    var label = document.createElement("label");
+    var input = document.createElement("input");
+    var submit = document.createElement("input");
+
+    input.type = "text";
+    submit.type = "submit";
+    submit.value = "Send";
+
+    label.innerHTML = "Enter your name";
+
     frame.innerHTML = null;
+
+    frame.appendChild(form);
+
+    form.appendChild(label);
+    form.appendChild(input);
+    form.appendChild(submit);
+
+    submit.onclick = function () {
+        return false;
+    }
 }
